b7898c43bca3274fa25c51963a088958db3c3e52
ayusshrathore <ayusshrathore@gmail.com>
2023-05-30 22:46:11 +0530

    chore: add payment controller & routes

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-var db = require('./server/config/db');
-const morgan = require('morgan');
-const bodyparser = require('body-parser');
-const cookieparser = require('cookie-parser');
+var db = require("./server/config/db");
+const morgan = require("morgan");
+const bodyparser = require("body-parser");
+const cookieparser = require("cookie-parser");
 var router = express.Router();
-router = require('./server/routes/index');
-const cors = require('cors');
-require('dotenv').config();
+router = require("./server/routes/index");
+const cors = require("cors");
+require("dotenv").config();
 
 //midlewares
-app.use(morgan('dev'));
-app.use(bodyparser.json({limit: '50mb'}));
-app.use(bodyparser.urlencoded({
-  limit: '50mb',
-  extended: true,
-  parameterLimit:50000
-}));
+app.use(morgan("dev"));
+app.use(bodyparser.json({ limit: "50mb" }));
+app.use(
+	bodyparser.urlencoded({
+		limit: "50mb",
+		extended: true,
+		parameterLimit: 50000,
+	})
+);
 app.use(cookieparser());
 app.use(cors());
-
 
 // DB Connection
 router.topicrouter(app, db);
@@ -34,20 +35,21 @@ router.subjectrouter(app, db);
 router.authrouter(app, db);
 router.blogsroute(app, db);
 router.userquestionsbookmark(app, db);
+router.paymentrouter(app, db);
 
 // server
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
 
 app.use((error, req, res, next) => {
-    console.log("app use error: ", error);
-    res.status(error.status || 500).send({
-      error: {
-        status: error.status || 500,
-        message: error.message || 'Internal Server Error',
-      },
-    });
+	console.log("app use error: ", error);
+	res.status(error.status || 500).send({
+		error: {
+			status: error.status || 500,
+			message: error.message || "Internal Server Error",
+		},
+	});
 });
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,74 +1,79 @@
-const authRoutes = require('./routes/authRoute');
-const chapterRoutes = require('./routes/chapterRoute');
-const courseRoutes = require('./routes/courseRoute');
-const notesRoutes = require('./routes/notesRoute');
-const packageRoutes = require('./routes/packageRoute');
-const questionRoutes = require('./routes/questionRoute');
-const subjectRoutes = require('./routes/subjectRoute');
-const testoRoutes = require('./routes/testoRoute');
-const testRoutes = require('./routes/testRoute');
-const topicRoutes = require('./routes/topicRoute');
-const blogsRoute = require('./routes/blogsRoute');
-const userQuestionsBookmark = require('./routes/userQuestionsBookmark');
+const authRoutes = require("./routes/authRoute");
+const chapterRoutes = require("./routes/chapterRoute");
+const courseRoutes = require("./routes/courseRoute");
+const notesRoutes = require("./routes/notesRoute");
+const packageRoutes = require("./routes/packageRoute");
+const questionRoutes = require("./routes/questionRoute");
+const subjectRoutes = require("./routes/subjectRoute");
+const testoRoutes = require("./routes/testoRoute");
+const testRoutes = require("./routes/testRoute");
+const topicRoutes = require("./routes/topicRoute");
+const blogsRoute = require("./routes/blogsRoute");
+const userQuestionsBookmark = require("./routes/userQuestionsBookmark");
+const paymentRoutes = require("./routes/payment");
 
 // Add access to the app and db objects to each route
 
 function authrouter(app, db) {
-  return authRoutes(app, db);
+	return authRoutes(app, db);
 }
 function courserouter(app, db) {
-  return courseRoutes(app, db);
+	return courseRoutes(app, db);
 }
 
 function notesrouter(app, db) {
-  return notesRoutes(app, db);
+	return notesRoutes(app, db);
 }
 
 function packagerouter(app, db) {
-  return packageRoutes(app, db);
+	return packageRoutes(app, db);
 }
 
 function subjectrouter(app, db) {
-  return subjectRoutes(app, db);
+	return subjectRoutes(app, db);
 }
 function questionrouter(app, db) {
-  return questionRoutes(app, db);
+	return questionRoutes(app, db);
 }
 
 function testorouter(app, db) {
-  return testoRoutes(app, db);
+	return testoRoutes(app, db);
 }
 
 function testrouter(app, db) {
-  return testRoutes(app, db);
+	return testRoutes(app, db);
 }
 
 function topicrouter(app, db) {
-  return topicRoutes(app, db);
+	return topicRoutes(app, db);
 }
 function chapterrouter(app, db) {
-  return chapterRoutes(app, db);
+	return chapterRoutes(app, db);
 }
 function blogsroute(app, db) {
-  return blogsRoute(app, db);
+	return blogsRoute(app, db);
 }
 
-function userquestionsbookmark (app, db) {
-  return userQuestionsBookmark(app, db);
+function userquestionsbookmark(app, db) {
+	return userQuestionsBookmark(app, db);
 }
 
+function paymentrouter(app, db) {
+	return paymentRoutes(app, db);
+}
 
 module.exports = {
-  chapterrouter,
-  topicrouter,
-  packagerouter,
-  testorouter,
-  testrouter,
-  questionrouter,
-  notesrouter,
-  courserouter,
-  authrouter,
-  subjectrouter,
-  blogsroute,
-  userquestionsbookmark
+	chapterrouter,
+	topicrouter,
+	packagerouter,
+	testorouter,
+	testrouter,
+	questionrouter,
+	notesrouter,
+	courserouter,
+	authrouter,
+	subjectrouter,
+	blogsroute,
+	userquestionsbookmark,
+	paymentrouter,
 };
